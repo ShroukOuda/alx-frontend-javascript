11a6cdcf585abb988d2962f2741f1119e7a676ea
ShroukOuda <s36568961@gmail.com>
2024-10-03 14:08:05 +0300

    first submit

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,3 @@
+export default (initialNumber, expansion1989, expansion2019) => 
+    initialNumber + ((expansion1989 === undefined) ? 89 : expansion1989) + ((expansion2019 === undefined) ? 19 : expansion2019);
+  
