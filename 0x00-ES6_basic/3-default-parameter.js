3ff576891b5d771b0f3d896b39825cc4ff08100a
ShroukOuda <s36568961@gmail.com>
2024-10-04 09:32:30 +0300

    remove line from 3-default-parameter file

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,4 +1,3 @@
 /* eslint-disable */
-export default (initialNumber, expansion1989, expansion2019) => 
-    initialNumber + ((expansion1989 === undefined) ? 89 : expansion1989) + ((expansion2019 === undefined) ? 19 : expansion2019);
+export default (initialNumber, expansion1989, expansion2019) => initialNumber + ((expansion1989 === undefined) ? 89 : expansion1989) + ((expansion2019 === undefined) ? 19 : expansion2019);
 /* eslint-disable */
